2cf903efd125d0c2d53b145e63771af7aeab0795
aringo <ringo.aaron@gmail.com>
2020-07-28 22:43:40 -0500

    added tamperscript
    Simple but effective at keeping session

---
+++ autook.js
@@ -0,0 +1,18 @@
+﻿// ==UserScript==
+// @name         SynackClick
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Will auto OK all PopUps on platform
+// @author       You
+// @match        https://platform.synack.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.confirm = () => {
+    return true
+    }
+
+})();
